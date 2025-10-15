eac01c8c69fad2287e4c9415e77e729f96e11923
JFGARURR <jfgarurr@alumno.upv.es@ldsic-vdi14.upvnet.upv.es>
2025-10-15 15:49:41 +0200

    Añadido el cpp y corregido del .h de Point2D

--- Point2D.h
+++ Point2D.h
@@ -10,25 +10,11 @@ class Point2D{
 		double x;
 		double y;
 		
-		Point2D(double x=0, double y=0): x(x), y(y) {}
-
-		static double distance(const Point2D &a, const Point2D &b){
-			return sqrt(pow(a.x - b.x,2) + pow(a.y - b.y, 2));
-		}
-		
-
-		friend bool operator==(const Point2D &a, const Point2D&b){
-			return (a.x == b.x && a.y == b.y);
-		}
-
-		friend bool operator!=(const Point2D &a, const Point2D &b){
-			return (a.x != b.x || a.y != b.y);
-		}
-
-		friend ostream& operator<<(ostream &out, const Point2D &p){
-			out << "("<< p.x << ", " << p.y << ")";
-			return out;
-		}
+		Point2D(double x=0, double y=0);
+		static double distance(const Point2D &a, const Point2D &b);
+		friend bool operator==(const Point2D &a, const Point2D&b);
+		friend bool operator!=(const Point2D &a, const Point2D &b);
+		friend ostream& operator<<(ostream &out, const Point2D &p);
 
 };
 
